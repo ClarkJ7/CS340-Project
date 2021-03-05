c9d4884c791ee05422302e598bffe6e4ff30c4ef
James Clark <J.Clark.NW@Gmail.com>
2021-03-04 20:47:19 -0800

    Updated 3/4

--- search.js
+++ search.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require('./dbcon.js');
 var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended:true}));
 
 router.all('/*', function (req, res, next) {
     req.app.locals.layout = 'search_layout'; // set your layout here
@@ -11,28 +13,65 @@ router.get('/', function (req, res) {
   res.render('blank');
 });
 
-router.get('/movieSearch', function (req, res) {
+router.post('/movieSearch', function (req, res) {
+	console.log("Title: " + req.body.titleSearch)
+	console.log("ID: " + req.body.idSearch)
+	console.log("Director: " + req.body.directorSearch)
 
-	console.log("Movie ID: " + req.query.idSearch)
-	console.log("Movie Title: " + req.query.titleSearch)
-	console.log("Director: " + req.query.directorSearch)
 
-    var movieID = req.query.idSearch;
-    context = {};
-    //var titleSearch = req.body.titleSearch;
-    //var directorSearch = req.body.directorSearch;
-    
-    var sql = "SELECT * FROM movies WHERE movie_id = ?";
+	if (!req.body.titleSearch) {
+		var titleSearch = null
+	} else {
+		var titleSearch = req.body.titleSearch
+	};
 
-    mysql.pool.query(sql, [movieID], function(err, rows, fields) {
-		if(err){
-      		next(err);
-      	return;
-    }
-    context.movies = rows;
-    res.render('search_movieid', context);
-    });
-  });
+	if (!req.body.idSearch) {
+		var idSearch = null
+	} else {
+		var idSearch = req.body.idSearch
+	};
+
+	if (!req.body.directorSearch) {
+		var directorSearch = null 
+	} else {  
+		var directorSearch = req.body.directorSearch
+	};
+
+	context = {};
+
+	if (req.body.idSubmit) {
+		var sql = "SELECT * FROM movies WHERE movie_id = ?";
+    	mysql.pool.query(sql, [idSearch], function(err, rows, fields) {
+			if(err){
+      			next(err);
+      		return;
+    		}
+    		context.movies = rows;
+    		res.render('search_movieid', context);
+    })};
+
+    if (req.body.titleSubmit) {
+		var sql = "SELECT * FROM movies WHERE movie_title LIKE ?";
+    	mysql.pool.query(sql, ['%' + titleSearch + '%'], function(err, rows, fields) {
+			if(err){
+      			next(err);
+      		return;
+    		}
+    		context.movies = rows;
+    		res.render('search_movieid', context);
+    })};
+  	
+	if (req.body.directorSubmit) {
+		var sql = "SELECT * FROM movies WHERE director LIKE ?";
+    	mysql.pool.query(sql, ['%' + directorSearch + '%'], function(err, rows, fields) {
+			if(err){
+      			next(err);
+      		return;
+    		}
+    		context.movies = rows;
+    		res.render('search_movieid', context);
+    })};
+});
 
 router.get('/actorSearch', function (req, res) {
 
@@ -41,9 +80,9 @@ router.get('/actorSearch', function (req, res) {
 
     context = {};
     
-    var sql = "SELECT * FROM actors WHERE actor_name = ?";
+    var sql = "SELECT * FROM actors WHERE actor_name LIKE ?";
 
-    mysql.pool.query(sql, [actorName], function(err, rows, fields) {
+    mysql.pool.query(sql, ['%' + actorName + '%'], function(err, rows, fields) {
 		if(err){
       		next(err);
       	return;
@@ -61,9 +100,9 @@ router.get('/characterSearch', function (req, res) {
     context = {};
 
     
-    var sql = "SELECT * FROM characters WHERE character_name = ?";
+    var sql = "SELECT * FROM characters WHERE character_name LIKE ?";
 
-    mysql.pool.query(sql, [charName], function(err, rows, fields) {
+    mysql.pool.query(sql, ['%' + charName + '%'], function(err, rows, fields) {
 		if(err){
       		next(err);
       	return;
